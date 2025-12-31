6ea136ef676ed737c08a053e876b626f4749eeeb
Beartropy <beartropy@gmail.com>
2025-12-31 13:22:22 -0300

    Update ManageRecipients.php

--- src/Livewire/ManageRecipients.php
+++ src/Livewire/ManageRecipients.php
@@ -47,7 +47,7 @@ class ManageRecipients extends YATBaseTable
     public $recipientIsActive;
     public $types;
     public $channels;
-    
+
     public $openDeleteConfirmationModal = false;
     public $recipientToDeleteId;
 
@@ -56,7 +56,8 @@ class ManageRecipients extends YATBaseTable
      *
      * @return void
      */
-    public function settings(): void {
+    public function settings(): void
+    {
         $this->tableName = trans('alert-system::messages.recipients');
         $this->setTitle(trans('alert-system::messages.recipients'));
         $this->setLayout(config('alert-system.layout'));
@@ -79,32 +80,33 @@ class ManageRecipients extends YATBaseTable
      *
      * @return array<int, \Beartropy\Tables\Classes\Columns\Column>
      */
-    public function columns(): array {
+    public function columns(): array
+    {
         return [
-            Column::make(trans('alert-system::messages.columns.id'),'id')
+            Column::make(trans('alert-system::messages.columns.id'), 'id')
                 ->isVisible(false)
                 ->collapseOnMobile(true),
-            Column::make(trans('alert-system::messages.columns.type'),'type.name'),
-            Column::make(trans('alert-system::messages.columns.channel'),'channel.name'),
-            Column::make(trans('alert-system::messages.columns.address'),'address'),
-            Column::make(trans('alert-system::messages.columns.bot'),'bot'),
-            BoolColumn::make(trans('alert-system::messages.columns.active'),'is_active'),
+            Column::make(trans('alert-system::messages.columns.type'), 'type.name'),
+            Column::make(trans('alert-system::messages.columns.channel'), 'channel.name'),
+            Column::make(trans('alert-system::messages.columns.address'), 'address'),
+            Column::make(trans('alert-system::messages.columns.bot'), 'bot'),
+            BoolColumn::make(trans('alert-system::messages.columns.active'), 'is_active'),
             Column::make('#')->customData(function ($row, $value) {
                 return '
-                <span wire:click="editRecipient('.$row->id.')">
+                <span wire:click="editRecipient(' . $row->id . ')">
                     <svg class="w-5 h-5 text-blue-400 dark:text-blue-600 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
                     </svg>
                 </span>
-                <span wire:click="confirmDeleteRecipient('.$row->id.')" class="ml-1">
+                <span wire:click="confirmDeleteRecipient(' . $row->id . ')" class="ml-1">
                     <svg class="w-5 h-5 text-red-400 dark:text-red-600 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
                     </svg>
                 </span>';
             })
-            ->toHtml()
-            ->thStyling('flex justify-end')
-            ->styling('flex justify-end')
+                ->toHtml()
+                ->thStyling('flex justify-end')
+                ->styling('flex justify-end')
         ];
     }
 
@@ -113,7 +115,8 @@ class ManageRecipients extends YATBaseTable
      *
      * @return array
      */
-    public function filters(): array {
+    public function filters(): array
+    {
         return [];
     }
 
@@ -122,7 +125,8 @@ class ManageRecipients extends YATBaseTable
      *
      * @return array<string, string>
      */
-    protected function rules() {
+    protected function rules()
+    {
         return [
             'recipientType' => 'required|exists:alert_types,id',
             'recipientChannel' => 'required|exists:alert_channels,id',
@@ -137,7 +141,8 @@ class ManageRecipients extends YATBaseTable
      *
      * @return void
      */
-    public function openNewRecipientModal() {
+    public function openNewRecipientModal()
+    {
         $this->reset(['selectedRecipient', 'recipientAddress', 'recipientBot', 'recipientType', 'recipientChannel']);
         $this->recipientIsActive = true;
         $this->createRecipient = true;
@@ -149,7 +154,8 @@ class ManageRecipients extends YATBaseTable
      *
      * @return void
      */
-    public function storeRecipient() {
+    public function storeRecipient()
+    {
         $this->validate();
         $recipient = AlertRecipient::create([
             'alert_type_id' => $this->recipientType,
@@ -158,9 +164,9 @@ class ManageRecipients extends YATBaseTable
             'bot' => $this->recipientBot,
             'is_active' => $this->recipientIsActive,
         ]);
-        
+
         $this->addRowToTable($recipient->toArray());
-        
+
         $this->openEditRecipientModal = false;
         $this->refresh();
     }
@@ -171,7 +177,8 @@ class ManageRecipients extends YATBaseTable
      * @param int|string $id The ID of the recipient
      * @return void
      */
-    public function editRecipient($id) {
+    public function editRecipient($id)
+    {
         $this->createRecipient = false;
         $this->selectedRecipient = AlertRecipient::find($id);
         $this->recipientType = $this->selectedRecipient->type->id;
@@ -179,7 +186,7 @@ class ManageRecipients extends YATBaseTable
         $this->recipientAddress = $this->selectedRecipient->address;
         $this->recipientBot = $this->selectedRecipient->bot;
         $this->recipientIsActive = $this->selectedRecipient->is_active ? true : false;
-        
+
         $this->openEditRecipientModal = true;
     }
 
@@ -188,8 +195,9 @@ class ManageRecipients extends YATBaseTable
      *
      * @return void
      */
-    public function updateRecipient() {
-        $this->validate();  
+    public function updateRecipient()
+    {
+        $this->validate();
         $this->selectedRecipient->alert_type_id  = $this->recipientType;
         $this->selectedRecipient->alert_channel_id = $this->recipientChannel;
         $this->selectedRecipient->address = $this->recipientAddress;
@@ -207,7 +215,8 @@ class ManageRecipients extends YATBaseTable
      * @param int|string $id The ID of the recipient to delete
      * @return void
      */
-    public function confirmDeleteRecipient($id) {
+    public function confirmDeleteRecipient($id)
+    {
         $this->recipientToDeleteId = $id;
         $this->openDeleteConfirmationModal = true;
     }
@@ -217,12 +226,13 @@ class ManageRecipients extends YATBaseTable
      *
      * @return void
      */
-    public function deleteRecipient() {
+    public function deleteRecipient()
+    {
         if ($this->recipientToDeleteId) {
             $recipient = AlertRecipient::find($this->recipientToDeleteId);
             if ($recipient) {
-                 $recipient->delete();
-                 $this->removeRowFromTable($this->recipientToDeleteId);
+                $recipient->delete();
+                $this->removeRowFromTable($this->recipientToDeleteId);
             }
         }
         $this->openDeleteConfirmationModal = false;
@@ -234,11 +244,12 @@ class ManageRecipients extends YATBaseTable
      *
      * @return array<string, array<string, string>>
      */
-    public function options(): array {
+    public function options(): array
+    {
         return [
-            "export_selected" => ["label"=>trans('alert-system::messages.actions.export_selected'), "icon"=> "☑️"],
-            "export_filtered" => ["label"=>trans('alert-system::messages.actions.export_filtered'), "icon"=> "🔍"],
-            "export_all" => ["label"=>trans('alert-system::messages.actions.export_all'), "icon"=> "📗"]
+            "export_selected" => ["label" => trans('alert-system::messages.actions.export_selected'), "icon" => "☑️"],
+            "export_filtered" => ["label" => trans('alert-system::messages.actions.export_filtered'), "icon" => "🔍"],
+            "export_all" => ["label" => trans('alert-system::messages.actions.export_all'), "icon" => "📗"]
         ];
     }
 
@@ -247,9 +258,10 @@ class ManageRecipients extends YATBaseTable
      *
      * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
      */
-    public function export_all() {
+    public function export_all()
+    {
         $allData = $this->getAllData();
-        return \Maatwebsite\Excel\Facades\Excel::download(new GenericExport($allData,$strip_tags = true), $this->tableName.'.xlsx');
+        return \Maatwebsite\Excel\Facades\Excel::download(new GenericExport($allData, $strip_tags = true), $this->tableName . '.xlsx');
     }
 
     /**
@@ -257,9 +269,10 @@ class ManageRecipients extends YATBaseTable
      *
      * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
      */
-    public function export_filtered() {
+    public function export_filtered()
+    {
         $filteredData = $this->getAfterFiltersData();
-        return \Maatwebsite\Excel\Facades\Excel::download(new GenericExport($filteredData, $strip_tags = true), $this->tableName.'.xlsx');
+        return \Maatwebsite\Excel\Facades\Excel::download(new GenericExport($filteredData, $strip_tags = true), $this->tableName . '.xlsx');
     }
 
     /**
@@ -267,11 +280,11 @@ class ManageRecipients extends YATBaseTable
      *
      * @return \Symfony\Component\HttpFoundation\BinaryFileResponse|void
      */
-    public function export_selected() {
+    public function export_selected()
+    {
         $selected_rows = $this->getSelectedData();
         if ($selected_rows) {
-            return \Maatwebsite\Excel\Facades\Excel::download(new GenericExport($selected_rows, $strip_tags = true), $this->tableName.'.xlsx');
+            return \Maatwebsite\Excel\Facades\Excel::download(new GenericExport($selected_rows, $strip_tags = true), $this->tableName . '.xlsx');
         }
     }
-}
 }
